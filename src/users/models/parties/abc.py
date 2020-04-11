0871fbc022c1822464f8199d9b7957ef3eb88ef8
Sergio García Prado <sergio@garciparedes.me>
2020-04-11 17:34:19 +0200

    ISSUE #17 * Included "id" field to "users.Party"

--- src/users/models/parties/abc.py
+++ src/users/models/parties/abc.py
@@ -3,7 +3,7 @@
 import logging
 
 from django.db.models import (
-    Model,
+    Model, AutoField,
 )
 
 logger = logging.getLogger(__name__)
@@ -12,10 +12,20 @@ logger = logging.getLogger(__name__)
 class Party(Model):
     """Party model."""
 
+    id = AutoField(primary_key=True)
+
     @property
     def is_supplier(self) -> bool:
+        """Flag to now if the party is a Supplier.
+
+        :return: A boolean value.
+        """
         return hasattr(self, "supplier")
 
     @property
     def is_client(self) -> bool:
+        """Flag to now if the party is a Client.
+
+        :return: A boolean value.
+        """
         return hasattr(self, "client")
